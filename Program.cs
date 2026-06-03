0c0fd302ef8d6a51a015781e8884545f766c442e
Rodrigo Sarain <amoresconsideratto@gmail.com>
2026-06-03 19:40:51 -0300

    feat: Dockerfile + CORS abierto para deploy en Railway

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("FrontendPolicy", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
